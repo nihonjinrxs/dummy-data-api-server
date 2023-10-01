8f2917d26032448d2c9ed189389f6a4f7e0bef42
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-10-01 16:21:46 -0500

    Formatting

--- index.js
+++ index.js
@@ -26,6 +26,5 @@ app.get('/data', (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
