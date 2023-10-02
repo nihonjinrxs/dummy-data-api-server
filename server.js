42d8a86577978e00a97ee3a95e939d6dd0fe9095
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-10-02 02:11:08 -0500

    Fix pagination to ensure the correct starting ID, date-based sorting, and count is returned

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const data = require('./data');
+const data = require('./data')
+  .sort((a, b) => {
+    return (a.last_reading < b.last_reading) ? -1 : (
+      (a.last_reading > b.last_reading) ? 1 : 0);
+  })
+  .map((record, index) => {
+    record.id = index + 1;
+    return record;
+  });
 
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 3000;
@@ -24,11 +32,11 @@ app.get('/data', (req, res) => {
   const maxPageSize = 500;
   const defaultPageSize = 100;
 
-  console.log(req.query);
-
   if (req.query.count) {
     queryCount = parseInt(req.query.count, 10);
-    if (isNaN(queryCount)) { queryCount = defaultPageSize; }
+    if (isNaN(queryCount)) {
+      queryCount = defaultPageSize;
+    }
     queryCount = Math.max(Math.min(queryCount, maxPageSize), 1);
   } else {
     queryCount = defaultPageSize;
@@ -36,13 +44,15 @@ app.get('/data', (req, res) => {
 
   if (req.query.from) {
     queryFrom = parseInt(req.query.from, 10);
-    if (isNaN(queryFrom)) { queryFrom = dataLength - queryCount; }
+    if (isNaN(queryFrom)) {
+      queryFrom = dataLength - queryCount + 1;
+    }
   } else {
-    queryFrom = dataLength - queryCount;
+    queryFrom = dataLength - queryCount + 1;
   }
 
   startId = Math.max(Math.min(queryFrom, dataLength), 1);
-  endId = Math.min(startId + queryCount, dataLength);
+  endId = Math.min(startId + queryCount, dataLength + 1);
 
   res.status(200).json({
     count: endId - startId,
@@ -54,7 +64,7 @@ app.get('/data', (req, res) => {
 
 async function closeGracefully (signal) {
   console.log(`Received signal to terminate: ${signal}`)
-  process.exit(1)
+  process.exit(1);
 }
 process.on('SIGINT', closeGracefully)
 process.on('SIGTERM', closeGracefully)
